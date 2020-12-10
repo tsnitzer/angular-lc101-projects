bdddaa2d28620a4bbe3f54b6fd603c0866eab741
tsnitzer <tleesnitzer@gmail.com>
2020-12-09 19:30:19 -0600

    added photos and updated formatting

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photoSectionTitle = 'Photo/Image Section Title Here';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://scx2.b-cdn.net/gfx/news/2015/kiwibirdgeno.jpg';
+  image3 = 'https://media.timeout.com/images/105232852/630/472/image.jpg';
 
   constructor() { }
 
